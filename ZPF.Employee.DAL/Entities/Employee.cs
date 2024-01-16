bb2fd4dfc59082a03c23bc970ce24580dc5706b0
алексей загородников <ayayv.ayayayv@yandex.ru>
2024-01-16 14:17:10 +0300

    Еhe initial architecture of the employee

---
+++ ZPF.Employee.DAL/Entities/Employee.cs
@@ -0,0 +1,11 @@
+﻿using ZPF.Employee.DAL.Entities.Base;
+
+namespace ZPF.Employee.DAL.Entities
+{
+    internal class Employee : NameBase
+    {
+        public string? LastName { get; set; }
+        public string? FirstName { get; set;}
+        public string? Position { get; set; }
+    }
+}
