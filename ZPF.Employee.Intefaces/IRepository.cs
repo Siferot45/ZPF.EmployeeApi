23329487bd6a03cd7d6fa6dab6829c6c27754bd6
алексей загородников <ayayv.ayayayv@yandex.ru>
2024-01-17 20:11:12 +0300

    Created the entity and repository interface

---
+++ ZPF.Employee.Intefaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ZPF.Employee.Interfaces
+{
+    public interface IRepository<T> where T : class, IEntity, new() 
+    { 
+        T GetValue(Guid id);
+        Task<T> GetByIdAsync(Guid id);
+    }
+}
