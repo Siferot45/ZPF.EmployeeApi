2f83f28bd01cc0da5cbad2c9091be6bd53232352
алексей загородников <ayayv.ayayayv@yandex.ru>
2024-01-19 13:22:35 +0300

    I made the repository interface

--- ZPF.Employee.Intefaces/IRepository.cs
+++ ZPF.Employee.Intefaces/IRepository.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ZPF.Employee.Interfaces
@@ -6,6 +9,23 @@ namespace ZPF.Employee.Interfaces
     public interface IRepository<T> where T : class, IEntity, new() 
     { 
         T GetValue(Guid id);
-        Task<T> GetByIdAsync(Guid id);
+
+        Task<T> GetByIdAsync(Guid id, CancellationToken cancel = default);
+
+        Task<IEnumerable<T>> GetEnumerableAsync(CancellationToken cancel = default);
+
+        Task<IQueryable<T>> GetQueryableAsync(CancellationToken cancel = default);
+
+        T Add(T entity);
+
+        Task<T> AddAsync(T entity, CancellationToken cancel = default);
+
+        void Update(T entity);
+
+        Task UpdateAsync(T entity, CancellationToken cancel = default);
+
+        void Delete(Guid id);
+
+        Task DeleteAsync(Guid id, CancellationToken cancel = default);
     }
 }
